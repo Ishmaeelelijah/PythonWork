ec51b06c3a225e70f3894567d675100535308dc5
Rhema Makamba <rhemaexel7@gmail.com>
2020-03-09 15:16:59 +0200

    this is my file

---
+++ MyDay.py
@@ -0,0 +1,11 @@
+import math
+a = int(input("Enter Value of a:"))
+b = int(input("Enter Value of b:"))
+c = int(input("Enter Value of c:"))
+x =  (math.sqrt(a-3))/((c**2)+(b**2))
+print(x)
+
+
+
+
+
